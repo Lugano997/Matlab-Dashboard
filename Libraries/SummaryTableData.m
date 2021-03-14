22c5ade8c5fd0b421e3551bb269b9f133905a982
Lugano997 <79929205+Lugano997@users.noreply.github.com>
2021-03-14 19:40:27 +0100

    Checking instrument correct
    Aggiunto controllo degli strumenti  nella  generazione del portafoglio indicizzato.
    -Da aggiungere  eliminazione vecchi portafogli.
    -Controllo scadenza

--- Libraries/SummaryTableData.m
+++ Libraries/SummaryTableData.m
@@ -2,9 +2,7 @@ function SummaryPortStatTable = SummaryTableData(PortfolioData,Currency)
     
     NewPortfolio = {};
     SummaryTable = table();
-for i = 1:size(PortfolioData,1)
-    Dates(i,1) = PortfolioData{i,1};
-end
+
 
     SummaryTable.Date = Dates;
 
@@ -34,30 +32,7 @@ end
 
 
 % CONTROLLO ALTRI GIORNI
-if size(PortfolioData,1) > 1
-else
-    
-    for i = 2:size(PortfolioData,1)    
-    
-    % Ottieni Dati Giorno Precedente
-    Instr1 = PortfolioData{i-1,4};
-    Pricer1 = PortfolioData{i-1,3};
-    Q1 = PortfolioData{i-1,2};
-    Currency1 = PortfolioData{i-1,5};
-    OptionType1 = PortfolioData{i-1,6};
-    CallPut1 = PortfolioData{i-1,7};
-    Strike1 = PortfolioData{i-1,8};
-    Maturity1 = PortfolioData{i-1,9};
-    
-    %Ottieni dati giorno i
-    Instr2 = PortfolioData{i,4};
-    Pricer2 = PortfolioData{i,3};
-    Q2 = PortfolioData{i,2};
-    Currency2 = PortfolioData{i,5};
-    OptionType2 = PortfolioData{i,6};
-    CallPut2 = PortfolioData{i,7};
-    Strike2 = PortfolioData{i,8};
-    Maturity2 = PortfolioData{i,9};
+
     
     
     CurrencyQ1 = Q1(Currency1 == Currency);
@@ -68,33 +43,11 @@ else
     CurrencyInstr2 = Instr2(Currency2 == Currency);
     CurrencyPricer = TempPricer(Currency2 == Currency);
 
-TEST = zeros(length(CurrencyInstr1),length(CurrencyInstr2));
-for i = 1:length(CurrencyInstr2)
-    for k = 1:length(CurrencyInstr1)
-        if CurrencyInstr2(i)== CurrencyInstr1(k)
-            TEST(i,k) = 1;
-        else
-            TEST(i,k) = 0;
-        end         
-    end
-end
 
-for i = 1:length(CurrencyQ2)
-UpdatedQuant(i) = sum([CurrencyQ2(i) CurrencyQ1(TEST(i,:))]);
-end
+
+
     
-    NewInstr = Instr1;
-    NewQ = CurrencyQ1;
-    NewInstADDED_Counter = 0;
-    for i = 1:length(Instr2)
-        if nnz(TEST(i,:)) == 0
-            NewInstr(length(NewInstr)+1) = Instr2(i);
-            NewPricer(length(NewPricer)+1) = Pricer2(i);
-            NewQ(length(NewQ)+1) = CurrencyQ2(i);
-            NewInstADDED_Counter = NewInstADDED_Counter +1; 
-        else
-           NewQ([TEST(i,:) logical(zeros([1 NewInstADDED_Counter]))) =  sum([CurrencyQ1(TEST(i,:)) CurrencyQ2(i)]);
-    end
+
         
     
     

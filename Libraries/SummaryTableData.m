1cd14cbf771e9726a50859d8d686ff56cc4944c0
Lugano997 <79929205+Lugano997@users.noreply.github.com>
2021-03-15 18:45:58 +0100

    Graphics update and picker limits

--- Libraries/SummaryTableData.m
+++ Libraries/SummaryTableData.m
@@ -26,49 +26,47 @@ function SummaryPortStatTable = SummaryTableData(PortfolioData,Currency)
     
     
     SummaryTable.NetPosition(1) = sum(TempQ(TempCurrency == Currency));
-    SummaryTable.LongNumber(1) = sum(TempQ(TempCurrency == Currency) > 0);
-    SummaryTable.ShortNumber(1) = sum(TempQ(TempCurrency == Currency) < 0);
+    SummaryTable.LongNumber(1) = sum(TempQ(TempCurrency == Currency & TempQ > 0));
+    SummaryTable.ShortNumber(1) = sum(TempQ(TempCurrency == Currency & TempQ < 0));
     SummaryTable.CallNumber(1) = sum(TempQ(TempCallPut == "CALL" & (TempCurrency == Currency)));
     SummaryTable.PutNumber(1) = sum(TempQ((TempCallPut == "PUT") & (TempCurrency == Currency)));
     SummaryTable.MeanExpiration(1) = mean(years(TempMaturity(TempCurrency == Currency)-DatesVector(1)));
-    SummaryTable.VanillaNumber(1) = prod(TempQ(TempOptionType(TempCurrency == Currency) == "VANILLA")); 
-    SummaryTable.BarrierNumber(1) = prod(TempQ(TempOptionType(TempCurrency == Currency) == "BARRIER")); 
-    SummaryTable.BinaryNumber(1) = prod(TempQ(TempOptionType(TempCurrency == Currency) == "BINARY")); 
-    SummaryTable.LookbackNumber(1) = prod(TempQ(TempOptionType(TempCurrency == Currency) == "LOOKBACK")); 
+    SummaryTable.VanillaNumber(1) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "VANILLA"))); 
+    SummaryTable.BarrierNumber(1) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "BARRIER"))); 
+    SummaryTable.BinaryNumber(1) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "BINARY"))); 
+    SummaryTable.LookbackNumber(1) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "LOOKBACK"))); 
 
 
 % CONTROLLO ALTRI GIORNI
 
 if size(PortfolioData,1) > 1    
-    
-    CurrencyQ1 = Q1(Currency1 == Currency);
-    CurrencyInstr1 = Instr1(Currency1 == Currency);
-    CurrencyPricer = TempPricer(Currency1 == Currency);
-    
-    CurrencyQ2 = Q2(Currency2 == Currency);
-    CurrencyInstr2 = Instr2(Currency2 == Currency);
-    CurrencyPricer = TempPricer(Currency2 == Currency);
-
-
-
-
-    
-
+    for i = 1:size(PortfolioData,1)
         
+    TempInstr = PortfolioData{i,2};
+    TempPricer = PortfolioData{i,3};
+    TempQ = PortfolioData{i,4};
+    TempCurrency = PortfolioData{i,5};
+    TempOptionType = PortfolioData{i,6};
+    TempCallPut = PortfolioData{i,7};
+    TempStrike = PortfolioData{i,8};
+    TempMaturity = PortfolioData{i,9};
     
     
+
     
-    SummaryTable.NetPosition(i) = sum(TempQ(TempCurrency == Currency));
-    SummaryTable.LongNumber(i) = nnz(TempQ(TempCurrency == Currency) > 0);
-    SummaryTable.ShortNumber(i) = nnz(TempQ(TempCurrency == Currency) < 0);
+    
+   SummaryTable.NetPosition(i) = sum(TempQ(TempCurrency == Currency));
+    SummaryTable.LongNumber(i) = sum(TempQ(TempCurrency == Currency & TempQ > 0));
+    SummaryTable.ShortNumber(i) = sum(TempQ(TempCurrency == Currency & TempQ < 0));
     SummaryTable.CallNumber(i) = sum(TempQ(TempCallPut == "CALL" & (TempCurrency == Currency)));
     SummaryTable.PutNumber(i) = sum(TempQ((TempCallPut == "PUT") & (TempCurrency == Currency)));
-    SummaryTable.MeanExpiration(i) = mean(years(TempMaturity(TempCurrency == Currency)-Dates(i)));
-    SummaryTable.VanillaNumber(i) = nnz(TempOptionType(TempCurrency == Currency) == "VANILLA"); 
-    SummaryTable.BarrierNumber(i) = nnz(TempOptionType(TempCurrency == Currency) == "BARRIER"); 
-    SummaryTable.BinaryNumber(i) = nnz(TempOptionType(TempCurrency == Currency) == "BINARY"); 
-    SummaryTable.LookbackNumber(i) = nnz(TempOptionType(TempCurrency == Currency) == "LOOKBACK"); 
+    SummaryTable.MeanExpiration(i) = mean(years(TempMaturity(TempCurrency == Currency)-DatesVector(i)));
+    SummaryTable.VanillaNumber(i) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "VANILLA"))); 
+    SummaryTable.BarrierNumber(i) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "BARRIER"))); 
+    SummaryTable.BinaryNumber(i) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "BINARY"))); 
+    SummaryTable.LookbackNumber(i) = sum(TempQ(TempOptionType(TempCurrency == Currency & TempOptionType == "LOOKBACK")));  
     
+    end
 end 
 
 

b449c90a8b8fd7e6ebb5099611694d49572adf04
Lugano997 <79929205+Lugano997@users.noreply.github.com>
2021-03-17 00:48:26 +0100

    Var Cocozza

---
+++ Libraries/GreekSensitivities.m
@@ -0,0 +1,41 @@
+function [Gamma, Delta, Prices, Maturities] =  GreekSensitivities(PriceMatrix, SelettoreSerieNum , SelettoreSerieChar, PortfolioData)
+
+[Instruments,Quantities,Pricer, Currency,~, ~ ,~ , ~] = ReadLatestPortfolio(PortfolioData);
+
+SelectedSerie = PriceMatrix(:,SelettoreSerieNum);
+Min = 0.6;
+Max = 0.8;
+
+Prices = linspace(Min,Max,100);
+Maturities = 30:30:365;
+
+Idx = Currency == SelettoreSerieChar;
+CurrencyInstr = Instruments(Idx);
+CurrencyPricer = Pricer(Idx);
+CurrencyQuantities = Quantities(Idx);
+
+Gamma = zeros(length(Prices),length(Maturities));
+Delta = zeros(length(Prices),length(Maturities));
+
+for j = 1:length(Prices)
+for k = 1:length(Maturities) 
+for i = 1:length(CurrencyInstr)
+    
+    CurrencyInstr(i).ExerciseDate = CurrencyInstr(i).ExerciseDate + Maturities(k);
+    CurrencyPricer(i).SpotPrice = Prices(j);
+    
+    
+end
+
+[~, ~, PortSens, ~] = pricePortfolio(finportfolio(CurrencyInstr,CurrencyPricer, CurrencyQuantities));
+Gamma(j,k) = PortSens.Gamma;
+Delta(j,k) = PortSens.Delta;
+end
+end
+
+
+
+
+
+
+end
